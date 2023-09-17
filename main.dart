7ceb408f14cce2becafb3504ab4aa29111e3cf66
fatima031 <fatimaabdulsamad16@gmail.com>
2023-09-17 22:05:20 +0500

    my new file

---
+++ main.dart
@@ -0,0 +1,129 @@
+void main() {
+//Question No. 1
+
+  // var list = ["Amna", "Ahmed", "Ahad", "Ali", "Alishba"];
+  // print(list);
+
+  //Question No. 2
+
+  // var listDay = [];
+  // listDay.add("Monday");
+  // listDay.add("Tuesady");
+  // listDay.add("Wednesday");
+  // listDay.add("Thursday");
+  // listDay.add("Friday");
+  // listDay.add("Saturday");
+  // listDay.add("Sunday");
+  // print(listDay);
+
+//Question No. 3
+
+  // var listday = [
+  //   "Monday",
+  //   "Tuesday",
+  //   " wednesday",
+  //   "Thursady",
+  //   "Friday",
+  //   "Saturday",
+  //   "Sunday"
+  // ];
+  // listday.removeAt(listday.length - 1);
+  // print(listday);
+
+//Question No. 4
+
+  // var list4 = [10, 1, 20, 15, 17, 0];
+  // list4.sort();
+  // print(list4);
+
+  // Question No. 5
+  // var mname = ["John", "Alice", "eligible", "Mike", "Sarah", "Tom"];
+  // mname.removeWhere((e) => e == "Alice" || e > "sarah");
+  // mname.retainWhere((e) => e == "Alice" || e > "sarah");
+  // print(mname);
+
+//Question No. 6
+  // var list6 = [25, 18, 1, 55, 25, 32, 25];
+  // list6.sort();
+  // print(list6);
+
+  //Question No. 7
+
+  // var fruitsList = [
+  //   "Apple",
+  //   "Mango",
+  //   "Banana",
+  //   "Grapes",
+  //   "Apple",
+  //   "Banana",
+  //   "Orange",
+  //   "Grapes"
+  // ];
+  // fruitsList.toList();
+
+//Question No. 8
+
+  // var list = ["Amna", "Alishba", "Alina", "Alizah", "Aisha"];
+  // var x = List.of(list.reversed);
+  // print(x);
+
+//Question No. 9
+  // var listRev = ["Ahmed", "Zunaib", "Taha", "umair", "Paras"];
+  // var listX = List.of(listRev.reversed);
+  // print(listX);
+
+//Question No. 10;
+
+//Questin No. 11
+
+  // var listNo = [126, 670, 290, 100, 287, 1000, 769, 480];
+  // listNo.sort();
+  // print(listNo);
+  // listNo.reversed;
+
+//Question No. 12
+
+  // var list0 = [12, -3, 5, 7, -9, 8, 6, -6];
+  // list0.removeWhere((e) => e < 0);
+  // print(list0);
+
+//Question No. 13
+  // var even = [2, 4, 6, 8, 10];
+  // var odd = [1, 3, 5, 7, 9, 11];
+  // if (even == 1) {
+  //   print("its even");
+  // } else {
+  //   print("its odd");
+  // }
+
+//Question No. 14
+
+  String listA = ["Apple", "Banana", "Orange", "Mango", "Peach"];
+   bool listB = [true, false, true, false, true, false,];
+  int listC = [11, 22, 33 ,44, 55, 66, 77 88];
+// print(listA.replaceRange(1, 3, 0));
+print(listB.replaceRange);
+print(listC.);
+
+
+
+//Question No. 15
+
+// var map1 = {1: 'Ahmed', 2 : 'Muzammil', 3 : "Mudassir"};
+// print(map1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
